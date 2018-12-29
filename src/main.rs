2511347c4b52683debb28b3957bdd2279ca5450b
raizul8 <bogarazvan2@gmail.com>
2018-12-29 20:14:23 +0200

    cleaning program

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ extern crate serde;
 extern crate serde_json;
 // use serde_json::*;
 mod gas_libs;
-use gas_libs::gas_validation::gas_validation as fv;
-// use gas_libs::static_variables_form::static_variables_form as STATIC;
+use gas_libs::gas_validation::gas_validation as gv;
+use gas_libs::static_variables_form::static_variables_form as STATIC;
 // use std::rc::Rc;
 
 extern crate cursive;
@@ -25,25 +25,22 @@ extern crate chrono;
 // use std::env;
 // use std::fs::File;
 // use std::io::Read;
-use std::iter::FromIterator;
-use gas_libs::gas_validation::gas_validation as gvalidation;
-use gas_libs::static_variables_form::static_variables_form as STATIC;
 
 // Start gui + logic
 fn cursive_linear() {
     let mut siv = Cursive::default();
 
-    fv::gas_entry_dialog(&mut siv);
-    fv::populate_form_with_date_and_c_price(&mut siv);
+    gv::gas_entry_dialog(&mut siv);
+    gv::populate_form_with_date_and_c_price(&mut siv);
 
     siv.run();
 }
 
 fn main() {
     cursive_linear();
-    let gas_entries = gvalidation::get_gas_entries(STATIC::GAS_DATA_JSON_FILE);
+    let gas_entries = gv::get_gas_entries(STATIC::GAS_DATA_JSON_FILE);
     let len10 = gas_entries.len() - 10;
-    let last_10_entries = Vec::from_iter(gas_entries[len10..].iter().cloned());
+    let last_10_entries: Vec<gv::GasEntry> = gas_entries[len10..].iter().cloned().collect();
 
     for g in last_10_entries.iter() {
         println!(
